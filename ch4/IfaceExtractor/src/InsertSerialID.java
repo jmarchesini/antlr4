d0f48014bbecc6314fea7bec10959ebe103c4f48
jmarchesini <jmarchesini@idauto.net>
2021-07-05 10:22:39 -0500

    Add SerialID

---
+++ ch4/IfaceExtractor/src/InsertSerialID.java
@@ -0,0 +1,35 @@
+/*
+ * Excerpted from "The Definitive ANTLR 4 Reference",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
+ */
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class InsertSerialID {
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if ( args.length>0 )
+            inputFile = args[0];
+
+        CharStream input = CharStreams.fromFileName(inputFile);
+        JavaLexer lexer = new JavaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokens);
+        ParseTree tree = parser.compilationUnit(); // parse
+
+        ParseTreeWalker walker = new ParseTreeWalker(); // create standard walker
+        InsertSerialIDListener extractor = new InsertSerialIDListener(tokens);
+        walker.walk(extractor, tree); // initiate walk of tree with listener
+
+        // print back ALTERED stream
+        System.out.println(extractor.rewriter.getText());
+    }
+}
